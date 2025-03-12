4f7fc9631cf72c2358314ba716f48c1dd04273e3
komyatminko <mgmyatminko@gmail.com>
2025-03-12 15:17:12 +0630

    keep only library-frontend

--- library-backend/src/main/java/com/myat/java/springBoot/library/dao/AuthorDao.java
+++
@@ -1,9 +0,0 @@
-package com.myat.java.springBoot.library.dao;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.myat.java.springBoot.library.model.Author;
-
-public interface AuthorDao extends ReactiveMongoRepository<Author, String> {
-
-}
--- library-backend/src/main/java/com/myat/java/springBoot/library/service/BorrowingService.java
+++
@@ -1,7 +0,0 @@
-package com.myat.java.springBoot.library.service;
-
-public interface BorrowingService {
-
-	
-	
-}
